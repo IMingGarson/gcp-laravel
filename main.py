9f07a7a0a807ec46ddf5e976e8a690369b19eb79
iminghsu <iminghsu@cw.com.tw>
2022-03-03 18:08:42 +0800

    feat: add test roure

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Flask Dockerized'
 
+@app.route('/test')
+def hello_world():
+    return 'Flask Dockerized from test'
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888)
